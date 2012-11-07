f7964b284f11a189668ee713578ffac8c57ee554
Augusto Rocha Elesbão <aelesbao@gmail.com>
2012-11-07 16:30:42 -0200

    Changed git branch info on custom prompt to be a val instead of a function

--- CustomPrompt.scala
+++ CustomPrompt.scala
@@ -8,11 +8,11 @@ object CustomPrompt extends Plugin {
     def buffer[T] (f: => T): T = f
   }
 
-  def currentBranch = (
+  lazy val currentBranch = (
     ("git symbolic-ref HEAD" lines_! devnull headOption) getOrElse "-" stripPrefix "refs/heads/"
   )
 
-  override def globalSettings = Seq(
+  override def settings = Seq(
     shellPrompt := { state =>
       val currentProject = Project.extract(state).currentProject.id
       "%s (%s)> ".format(currentProject, currentBranch)
